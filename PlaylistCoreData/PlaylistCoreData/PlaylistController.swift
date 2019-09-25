9eb724a980af9502b1918b265b2a82300e4f7b19
Josh Sparks <joshsparks@Joshs-MacBook-Pro.local>
2019-09-25 12:27:16 -0600

    Completed Model Controllers

--- PlaylistCoreData/PlaylistCoreData/PlaylistController.swift
+++ PlaylistCoreData/PlaylistCoreData/PlaylistController.swift
@@ -7,24 +7,36 @@
 //
 
 import Foundation
-
+import CoreData
 class PlaylistController {
+    
+    //Singleton
+    static let sharedInstance = PlaylistController()
+    //Local Source of Truth
+    var playlists: [Playlist] {
+        
+            let fetchRequest: NSFetchRequest<Playlist> = Playlist.fetchRequest()
+            return (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
+    }
     // CRUD
     //Create
     func createPlaylist(withName name: String) {
+        _ = Playlist(playlistName: name)
+        saveToPersistentStore()
         
     }
     //Delete
     func deletePlaylist(playlist: Playlist) {
-        
+        CoreDataStack.context.delete(playlist)
+        saveToPersistentStore()
     }
     //Save
     func saveToPersistentStore() {
         do {
             try CoreDataStack.context.save()
         } catch {
-                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
-            }
+            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
         }
     }
+} // end of class
 
